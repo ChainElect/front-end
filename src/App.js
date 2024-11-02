fe7209048d1beab688c353afcd750f50fc38e77d
Ivailo Hasov <ivailohasov@gmail.com>
2024-11-02 20:13:06 +0200

    Connecting the Web3 Wallet to the app

--- src/App.js
+++ src/App.js
@@ -4,11 +4,35 @@ import LoginScreen from './components/Auth/Login';
 import RegistrationForm from './components/Auth/Register';
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import { init, useConnectWallet } from "@web3-onboard/react";
+import injectedModule from "@web3-onboard/injected-wallets";
+import { ethers } from "ethers";
+import injectedModules from '@web3-onboard/injected-wallets';
+
+const API_KEY = "89jSnaKWM_MxViGgmQyrTyS4xHMzXukz";
+const rpcUrl = `https://eth-sepolia.g.alchemy.com/v2/${API_KEY}`;
+
+const injected = injectedModules();
+
+init({
+  connect: {
+    autoConnectLastWallet: true,
+  },
+  wallets: [injected],
+  chains: [
+    {
+      id: "0xaa36a7",
+      token: "ETH",
+      label: "Ethereum Sepolia",
+      rpcUrl,
+    },
+  ],
+});
 
 function App() {
   return (
     <Router>
-      <Navbar/>
+      <Navbar />
       <div className="min-h-screen bg-gray-100 flex items-center justify-center">
         <Routes>
           <Route path="/login" element={<LoginScreen />} />
@@ -16,7 +40,7 @@ function App() {
           <Route path="/connect" element={<RegistrationForm />} />
         </Routes>
       </div>
-      <Footer/>
+      <Footer />
     </Router>
   );
 }
