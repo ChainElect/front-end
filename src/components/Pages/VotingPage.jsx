23bd6f5a133ac095764aef16af4da21dcab07550
BorislavChernev <borocool359@gmail.com>
2024-11-17 14:09:37 +0200

    fixed register page

--- src/components/Pages/VotingPage.jsx
+++ src/components/Pages/VotingPage.jsx
@@ -74,7 +74,7 @@ const VotingPage = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div className="min-h-screen bg-gray-100 mt-10 mb-10">
       {/* Header Section */}
       <header className="bg-purple-600 text-white py-12">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center">
