5aa62aee60b7ba5a02d32c3827b84d4137a782de
BorislavChernev <borocool359@gmail.com>
2025-02-15 23:52:48 +0200

    altered import configs on RCA

---
+++ craco.config.js
@@ -0,0 +1,11 @@
+// craco.config.js
+const path = require("path");
+
+module.exports = {
+  webpack: {
+    alias: {
+      "@assets": path.resolve(__dirname, "src/assets/"),
+      "@components": path.resolve(__dirname, "src/components/"),
+    },
+  },
+};
